4b71586998c826a65f1b6a5207f1368078fcfd8a
Тіхон Іщенко <tikhon.ishchenko@ukma.edu.ua>
2022-12-30 16:46:53 +0200

    Created the needed class and all methods, added testing

--- PremiumCalculator.cs
+++ PremiumCalculator.cs
@@ -8,6 +8,59 @@ namespace TeambaseTechnicalTask
 {
     internal class PremiumCalculator
     {
-        
+
+        private readonly DateTime memberAdditionDate;
+        private readonly int memberAge;
+        private readonly char memberGender;
+        private readonly int flatRate = 1000;
+        private readonly float ageFactorWomen = 1.5f;
+        private readonly DateTime policyEndDate;
+        private readonly char prorateType;
+
+        public PremiumCalculator(DateTime memberAdditionDate, int memberAge, char memberGender, char prorateType)
+        {
+            this.memberAdditionDate = memberAdditionDate;
+            this.policyEndDate = new DateTime(memberAdditionDate.Year, 12, 31);
+            this.memberAge = memberAge;
+            this.memberGender = memberGender;
+            this.prorateType = prorateType;
+            
+        }
+
+        private int AgeMultiplier()
+        {
+            int ageMultiplier = memberAge / 10;
+            return (ageMultiplier + 1) * 100;
+        }
+
+        private float GenderMultiplier()
+        {
+            if (memberGender == 'f' && memberAge > 18)
+                return ageFactorWomen;
+            return 1;
+        }
+
+        private double CalculateFullPremium()
+        {
+            return flatRate * AgeMultiplier() * GenderMultiplier();
+        }
+
+        public Tuple<double, double> CalculateProratedPremium()
+        {
+            if (prorateType == 'd')
+            {
+                int daysInPolicy = (policyEndDate - memberAdditionDate).Days + 1;
+                return Tuple.Create(CalculateFullPremium(), CalculateFullPremium() * daysInPolicy / 365); ;
+            }
+            else if (prorateType == 'm')
+            {
+                int monthsInPolicy = (policyEndDate.Year - memberAdditionDate.Year) * 12 + policyEndDate.Month - memberAdditionDate.Month + 1;
+                return Tuple.Create(CalculateFullPremium() , CalculateFullPremium() * monthsInPolicy / 12);
+            }
+            else
+            {
+                return Tuple.Create(CalculateFullPremium(), 0d);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using TeambaseTechnicalTask;
+
+PremiumCalculator premiumCalculator = new PremiumCalculator(new DateTime(2021, 3, 1), 25, 'f', 'd');
+
+Console.WriteLine("A woman of 25 years that joined the company on 2021.03.01 prorated by days"+premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 7, 12), 12, 'f', 'm');
+
+Console.WriteLine("A woman of 12 years that joined the company on 2021.07.12 prorated by months" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 5, 23), 44, 'm', 'd');
+
+Console.WriteLine("A man of 44 years that joined the company on 2021.05.23 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+
+premiumCalculator = new PremiumCalculator(new DateTime(2023, 2, 24), 20, 'f', 'd');
+
+Console.WriteLine("A woman of 20 years that joined the company on 2023.02.24 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2023, 2, 24), 29, 'f', 'd');
+
+Console.WriteLine("A woman of 29 years that joined the company on 2023.02.24 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 1, 28), 31, 'm', 'm');
+
+Console.WriteLine("A man of 31 years that joined the company on 2021.01.28 prorated by months" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 18, 'm', 'd');
+
+Console.WriteLine("A man of 18 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 18, 'f', 'd');
+
+Console.WriteLine("A woman of 18 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 12, 'm', 'd');
+
+Console.WriteLine("A man of 12 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 12, 'f', 'd');
+
+Console.WriteLine("A woman of 12 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 19, 'm', 'd');
+
+Console.WriteLine("A man of 19 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
+
+premiumCalculator = new PremiumCalculator(new DateTime(2021, 4, 16), 19, 'f', 'd');
+
+Console.WriteLine("A woman of 19 years that joined the company on 2021.04.16 prorated by days" + premiumCalculator.CalculateProratedPremium());
